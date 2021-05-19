276b90ad6e5ff604a5a9725cded55744b8d8e089
Carl <carlbrainegebe@gmail.com>
2021-05-19 17:31:34 -0500

    commit tree ab728787d56c281677be5fd8b966876ed552df00

---
+++ index.js
@@ -0,0 +1,14 @@
+// const players = ["Kyler", "Hopkins", "Green"];
+
+// const offenseIntro = players.map( (element) => {return `${element} plays for the Arizona Cardinals.`} );
+
+// console.log(offenseIntro);
+
+function divide() {
+
+   return 2000 / 100;
+}
+
+const square = (num1) => num1 * num1;
+
+const add = (num2, num3) => {return num2 + num3;}
